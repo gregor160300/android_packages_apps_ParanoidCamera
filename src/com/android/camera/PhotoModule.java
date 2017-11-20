AOSPA/android_packages_apps_SnapdragonCamera

PhotoModule: Support preview restart on manual iso
For some devices we need to restart the preview when
manual iso is used.
This patch detects the manual iso mode and decides whether
to restart the preview based on that.

Change-Id: I3c6b1a98510a950010ebecba528dee9fa641247f
Signed-off-by: Alex Naidis <alex.naidis@linux.com>

src/com/android/camera/PhotoModule.java
@@ -1447,10 +1447,20 @@ public class PhotoModule
                 mApplicationContext.getResources().getBoolean(R.bool.back_camera_restart_preview_onPictureTaken);
             boolean frontCameraRestartPreviewOnPictureTaken =
                 mApplicationContext.getResources().getBoolean(R.bool.front_camera_restart_preview_onPictureTaken);
+            boolean manualIsoRestartPreviewOnPictureTaken =
+                mApplicationContext.getResources().getBoolean(R.bool.manual_iso_restart_preview_onPictureTaken);
 
             CameraInfo info = CameraHolder.instance().getCameraInfo()[mCameraId];
+
+            final String isoPref = mPreferences.getString(
+                    CameraSettings.KEY_ISO,
+                    mActivity.getString(R.string.pref_camera_iso_default));
+
+            final boolean isManualIso = !isoPref.equals("auto");
+
             if ((info.facing == CameraInfo.CAMERA_FACING_BACK && backCameraRestartPreviewOnPictureTaken)
-                    || (info.facing == CameraInfo.CAMERA_FACING_FRONT && frontCameraRestartPreviewOnPictureTaken)) {
+                    || (info.facing == CameraInfo.CAMERA_FACING_FRONT && frontCameraRestartPreviewOnPictureTaken)
+                    || (isManualIso && manualIsoRestartPreviewOnPictureTaken)) {
                 needRestartPreview = true;
             }
 

gregor160300/android_packages_apps_ParanoidCamera

src/com/android/camera/PhotoModule.java
@@ -1519,10 +1519,20 @@ public class PhotoModule
                     mActivity.getResources().getBoolean(R.bool.back_camera_restart_preview_onPictureTaken);
             boolean frontCameraRestartPreviewOnPictureTaken =
                     mActivity.getResources().getBoolean(R.bool.front_camera_restart_preview_onPictureTaken);
+            boolean manualIsoRestartPreviewOnPictureTaken =
+                    mActivity.getResources().getBoolean(R.bool.manual_iso_restart_preview_onPictureTaken);
 
             CameraInfo info = CameraHolder.instance().getCameraInfo()[mCameraId];
+
+            final String isoPref = mPreferences.getString(
+                    CameraSettings.KEY_ISO,
+                    mActivity.getString(R.string.pref_camera_iso_default));
+
+            final boolean isManualIso = !isoPref.equals("auto");
+
             if ((info.facing == CameraInfo.CAMERA_FACING_BACK && backCameraRestartPreviewOnPictureTaken)
-                    || (info.facing == CameraInfo.CAMERA_FACING_FRONT && frontCameraRestartPreviewOnPictureTaken)) {
+                    || (info.facing == CameraInfo.CAMERA_FACING_FRONT && frontCameraRestartPreviewOnPictureTaken)
+                    || (isManualIso && manualIsoRestartPreviewOnPictureTaken)) {
                 needRestartPreview = true;
             }
 